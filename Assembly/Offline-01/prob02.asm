278d527ca1a0aba440dd13abbbeb056e927a259e
Subangkar <subangkar.karmaker@gmail.com>
2018-04-10 17:34:30 +0600

    Prob02

---
+++ Assembly/Offline-01/prob02.asm
@@ -0,0 +1,65 @@
+
+.model small
+
+
+.data
+
+MSG DB ".....$"
+MSG1 DB "Input a uppercase character: $"
+MSG2 DB "The converted lowercase character is: $"
+
+x DB 0
+
+.code
+
+main proc    
+    
+    ; initialize Data Segment = AX
+    MOV AX,@DATA
+    MOV DS,AX
+
+    ; output string
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21h
+
+
+
+    
+	; take input
+    MOV AH,1
+    INT 21h
+	MOV x,AL
+    
+    ADD x,32
+
+    ; newline = AX , DX
+    MOV AH,2
+    MOV DL,0AH
+    INT 21h   
+    MOV DL,0DH
+    INT 21h 
+
+
+    ; output string
+    LEA DX,MSG2
+    MOV AH,9
+    INT 21h
+
+    MOV DL,x
+    MOV AH,2
+    INT 21h    
+    
+    
+    
+    
+    
+    mov     ah,4Ch       ;DOS function: Exit program 
+    mov     al,0         ;Return exit code value 
+    int     21h          ;Call DOS. Terminate program 
+       
+main endp    
+
+end main
+
+
