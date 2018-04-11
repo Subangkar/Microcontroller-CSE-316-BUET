0ad41a7ad171d53470a9ea335936824adef9fb05
Subangkar <subangkar.karmaker@gmail.com>
2018-04-11 12:50:35 +0600

    Updated

--- Assembly/Common Codes.asm
+++ Assembly/Common Codes.asm
@@ -15,13 +15,13 @@ main proc
     MOV AX,@DATA
     MOV DS,AX
     
-	; take input
+	; take input in AL affects AX
     MOV AH,1
     INT 21h
 	;MOV CL,AL
     
 
-    ; newline = AX , DX
+    ; newline = AX , DL
     MOV AH,2
     MOV DL,0AH
     INT 21h   
@@ -29,13 +29,13 @@ main proc
     INT 21h 
     
     
-    ; output string
+    ; output string affects AX,DX
     LEA DX,MSG
     MOV AH,9
     INT 21h
 
 
-    ; newline = AX , DX
+    ; newline = AX , DL
     MOV AH,2
     MOV DL,0AH
     INT 21h   
@@ -43,7 +43,7 @@ main proc
     INT 21h 
     
 
-	
+
 	;print a space
     MOV DL, 032
     MOV AH,2
@@ -54,16 +54,136 @@ main proc
 	
 
 
-
-
     
     mov     ah,4Ch       ;DOS function: Exit program 
     mov     al,0         ;Return exit code value 
     int     21h          ;Call DOS. Terminate program 
        
+
 	
 main endp    
 
 end main
 
 
+
+
+
+
+
+
+
+
+; if-else
+
+CMP cond
+JMP IFC
+JMP ELSC
+
+IFC:
+
+JMP END_IF_ELS
+
+ELSC:
+
+END_IF_ELS:
+
+
+
+
+
+
+
+
+
+
+
+; if - else if - else
+
+CMP cond_if
+JMP IFC
+CMP cond_els_if
+JMP ELS_IFC
+JMP ELSC
+
+IFC:
+
+JMP END_IF_ELSIF_ELS
+
+ELS_IFC:
+
+JMP END_IF_ELSIF_ELS
+
+ELSC:
+
+END_IF_ELSIF_ELS:
+
+
+
+
+
+
+
+
+
+
+
+; LOOP
+
+MOV CX,n
+
+LOOP_LABEL:
+
+LOOP LOOP_LABEL
+
+
+
+
+
+
+
+
+
+
+
+
+
+; REPEAT
+
+LOOP_LABEL:
+
+CMP cond_loop
+JMP LOOP_LABEL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; take input of multiple digit decimal number<255 affects AX,DX output in DH	
+MOV AL,'0'
+MOV DH,0  ; DH holds all digit number	
+SCAN_DIGITS:
+
+MOV DL,AL ; DL holds last digit char
+SUB DL,'0'; DL holds last digit value
+
+MOV AL,DH
+MOV AH,10
+MUL AH
+MOV DH,AL
+ADD DH,DL
+ 
+MOV AH,1
+INT 21h
+
+CMP AL,0DH
+JNE SCAN_DIGITS
