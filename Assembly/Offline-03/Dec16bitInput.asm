ff58380ef81413f08f1d0bc0d1d0beb2b2f03510
Subangkar [Gitkraken-HP-Ubuntu18] <subangkar.karmaker@gmail.com>
2018-05-06 22:10:59 +0600

    INDEC Fixed
    DL outputs 
     ASCII on non-digit (+ve)
     else -ve length of scanned valid  number

--- Assembly/Offline-03/Dec16bitInput.asm
+++ Assembly/Offline-03/Dec16bitInput.asm
@@ -6,21 +6,21 @@ INDEC PROC
 ; stops @ receiving x
 ; input : none
 ; output : store binary number in AX , other READ char in DL
-;           DL = 0 for space
+;           DL = 0 for ws
+;           DL = -l for valid number where l=scanned length
+;           DL = ascii of non-digit char else +ve
 ; uses : MAIN
 
   PUSH BX                        
-  PUSH CX                        
+  PUSH CX ;CH=sign,CL=scanned length inc +/-                
   ;PUSH DX                        
 
   JMP @READ                      
 
   @SKIP_BACKSPACE:               
-    MOV AH, 2                      
-    MOV DL, 20H                    ; set DL=\' \'
-    INT 21H                        
+    CALL INDEC_printSpace                        
 
-  @READ:                         
+  @READ: ; reads a char & checks whether it's sign otherwise input taken hence go to next (assuming +ve number)                   
     XOR BX, BX                     
     XOR CX, CX                     
     XOR DX, DX                     
@@ -35,7 +35,7 @@ INDEC PROC
 
     JMP @SKIP_INPUT                
 
-  @MINUS:                        
+  @MINUS: ; sets sign info @ CH                       
     MOV CH, 1                  ; CH=1 for - , 2 for - , 0 for nothing   
     INC CL                     ; CL=1    
     JMP @INPUT                     
@@ -44,36 +44,37 @@ INDEC PROC
     MOV CH, 2                      
     INC CL                         
 
-  @INPUT:                        
+  @INPUT: ; read a digit/anyother char except +/-                       
     CALL INDEC_scanByteIn_AL                     
 
-  @SKIP_INPUT:                 ; exits or ignores ws
+  @SKIP_INPUT:                 ; exits or ignores ws or put digit
 
-    XOR DL, DL                   ; DL = 0
-    CMP AL, ' '                  ; compare AL with CR
-    JE @END_INPUT                
+    @CHECK_WS:  ; if WS set DL=0 & finish input
+      XOR DL, DL                   ; DL = 0
+      CMP AL, ' '                  ; compare AL with CR
+      JE @END_INPUT                
 
-    XOR DL, DL                   ; DL = 0
-    CMP AL, 10                  ; compare AL with \n
-    JE @END_INPUT                
+      XOR DL, DL                   ; DL = 0
+      CMP AL, 10                  ; compare AL with \n
+      JE @END_INPUT                
 
-    XOR DL, DL                   ; DL = 0
-    CMP AL, 13                  ; compare AL with \r & print new line
-    JNE @NOT_PRINT_NEL_LINE
+      XOR DL, DL                   ; DL = 0
+      CMP AL, 13                  ; compare AL with \r & print new line
+      JNE @NOT_PRINT_NEL_LINE
     @PRINT_NEL_LINE :
     CALL INDEC_printNewline
     JMP @END_INPUT                
 
-    @NOT_PRINT_NEL_LINE:
+  @NOT_PRINT_NEL_LINE:
 
-
-    MOV DL, 'x'
-    CMP AL, 'x'                  ; compare AL with CR
-    JE @END_INPUT                
 
     XOR DL, DL                   ; DL = 0
     CMP AL, 08H                   ; compare AL with 8H
     JNE @NOT_BACKSPACE           
+
+;    MOV DL, 'x'
+;    CMP AL, 'x'                  ; compare AL with CR
+;    JE @END_INPUT                
 
     CMP CH, 0                    
     JNE @CHECK_REMOVE_MINUS      
@@ -112,7 +113,7 @@ INDEC PROC
 
     CALL INDEC_removeOneChar                    
 
-    XOR DX, DX                   ; DX = 0
+    ;XOR DX, DX                   ; DX = 0
     DEC CL                       
 
     JMP @INPUT                   
@@ -121,10 +122,10 @@ INDEC PROC
 
     INC CL                       
 
-    CMP AL, 30H                  
+    ; checks 0-9
+    CMP AL, '0'                  
     JL @ERROR                    
-
-    CMP AL, 39H                  
+    CMP AL, '9'                  
     JG @ERROR                    
 
     AND AX, 000FH                ; convert ascii to decimal code
@@ -141,27 +142,16 @@ INDEC PROC
     JMP @INPUT                     
 
   @ERROR:                        
-
-    MOV AH, 2                      
-    MOV DL, 7H                     
-    INT 21H                        
-
-    XOR CH, CH                     ; clear CH
-
-;  @CLEAR:                        
-;    MOV DL, 8H                   
-;    INT 21H                      
-
-;   CALL INDEC_removeOneChar                  
-    ;   LOOP @CLEAR                    
-
-    JMP @READ                      
+    MOV DL,AL                   ; put non digit char in DL always +ve
+    JMP @EXIT
 
   @END_INPUT:                    
 
     CMP CH, 1                      ; if neg
     JNE @EXIT                      
     NEG BX                         ; negate BX
+    MOV DL,CL
+    NEG DL
 
   @EXIT:                         
 
@@ -188,13 +178,19 @@ INDEC_scanByteIn_AL endp
 
 ; Affects AX,DL
 INDEC_removeOneChar proc
-    MOV AH, 2                    
-    MOV DL, ' '                  ; set DL=\' \'
-    INT 21H                      
+  PUSH AX
+  PUSH DX
 
-    MOV DL, 8H                   
-    INT 21H          
-    RET            
+  MOV AH, 2                    
+  MOV DL, ' '                  ; set DL=\' \'
+  INT 21H                      
+
+  MOV DL, 8H                   
+  INT 21H 
+
+  POP DX
+  POP AX         
+  RET            
 INDEC_removeOneChar endp
 
 
@@ -214,3 +210,20 @@ INDEC_printNewline proc
     POP AX
     RET
 INDEC_printNewline endp
+
+
+
+;print a space
+INDEC_printSpace proc
+    PUSH AX
+    PUSH DX
+
+    MOV DL, ' '
+    MOV AH,2
+    INT 21h
+
+    POP DX
+    POP AX
+INDEC_printSpace endp
+
+
