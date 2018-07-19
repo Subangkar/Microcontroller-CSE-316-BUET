b01160ccff59984dead10208ff57ebdb77f966a3
Subangkar [SourceTree-HP-Win10] <subangkar.karmaker@gmail.com>
2018-07-19 11:23:41 +0600

    Input : Not Sure Sampling : Ok DFT : Ok

--- AudioSpecViz/ClockConfig/ClockConfig/main.c
+++ AudioSpecViz/ClockConfig/ClockConfig/main.c
@@ -9,13 +9,16 @@
 #define F_CPU 1000000
 #include <util/delay.h>
 
+#define FREQ_TO_GEN 300
+#define usDELAY (1000000/FREQ_TO_GEN)
+
 int main(void)
 {
     DDRA=0xFF;
 	while (1) 
     {
 		PORTA = PORTA ^ 1;
-		_delay_us(250);
+		_delay_us(usDELAY);
     }
 }
 
